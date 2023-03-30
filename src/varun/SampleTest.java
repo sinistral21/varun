8f3a4ea8be330bef4764c06ad5d51a4d6d599d89
sachin212121 <105343329+sachin212121@users.noreply.github.com>
2023-03-30 11:11:07 +0530

    removed print statement

--- src/varun/SampleTest.java
+++ src/varun/SampleTest.java
@@ -5,7 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("sachin");
-	System.out.println("sachin2");	
+	
 	System.out.println("Sanjay");
 	}
 
